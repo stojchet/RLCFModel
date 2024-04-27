f2cda86a490a52b4c260ac126c5748438fed29b2
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-04-27 14:18:30 +0200

    Add truncation, passing params and fix pas token id error in model

---
+++ src/model_impl.py
@@ -0,0 +1,33 @@
+from typing import Union
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+class Model:
+    def __init__(self,
+                 name: str,
+                 torch_dtype: torch.dtype,
+                 max_new_tokens: int = 2000,
+                 padding: Union[bool, str] = False,
+                 truncation: Union[bool, str] = False) -> None:
+        print(torch_dtype)
+        self.name = name
+        self.max_new_tokens = max_new_tokens
+        self.padding = padding
+        self.truncation = truncation
+        self.model = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, torch_dtype=torch_dtype)
+        self.tokenizer = AutoTokenizer.from_pretrained(name, trust_remote_code=True, use_fast=True)
+
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def predict(self, prompt: str) -> str:
+        inputs = self.tokenizer(prompt, return_tensors="pt", return_attention_mask=False, truncation=self.truncation, padding=self.padding)
+
+        outputs = self.model.generate(**inputs,
+                                      max_new_tokens=self.max_new_tokens,
+                                      do_sample=True,
+                                      pad_token_id=self.tokenizer.pad_token_id
+                                      )
+        return self.tokenizer.batch_decode(outputs)[0][len(prompt):]
