b13c34d720d7a4a6bbe36c1bfd7413f3469d4537
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-05-08 09:21:07 +0200

    Add max input to mlp and concat code bert embeddings

--- src/cf_dataset/discriminator.py
+++ src/cf_dataset/discriminator.py
@@ -4,7 +4,7 @@ import torch
 from datasets import load_dataset, tqdm
 from torch import nn, optim
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModel, pipeline
+from transformers import AutoTokenizer, AutoModel
 
 torch.set_default_device("cuda")
 torch.set_float32_matmul_precision('medium')
@@ -36,6 +36,8 @@ parser.add_argument(
     default=1e-4,
 )
 
+MAX_FEATURE_SIZE = 10000
+
 
 class CodeBert:
     def __init__(self):
@@ -46,10 +48,22 @@ class CodeBert:
         self.tokenizer.add_special_tokens(special_tokens_dict)
         self.model.resize_token_embeddings(len(self.tokenizer))
 
-        self.pipe_model = pipeline('feature-extraction', model=self.model, tokenizer=self.tokenizer, truncation=True)
 
     def get_sentence_embedding(self, sentence: str) -> torch.Tensor:
-        return self.pipe_model(sentence)[0][0]
+        encoded = self.tokenizer.encode(sentence)
+        tokenized_sent = torch.tensor(encoded)
+
+        max_size = 512
+        final = []
+        while True:
+            embeddings = self.model(tokenized_sent[:max_size].unsqueeze(0))
+            embeddings = embeddings.last_hidden_state[0][0]
+            final += embeddings
+            if len(tokenized_sent) < max_size:
+                break
+            tokenized_sent = tokenized_sent[max_size:]
+
+        return torch.tensor(final)
 
 
 class MLP(nn.Module):
@@ -63,19 +77,19 @@ class MLP(nn.Module):
             nn.Linear(hidden_size, out)
         )
 
-    def forward(self, batch):
+    def forward(self, batch) -> torch.Tensor:
         return self.layers(batch)
 
 
 class Discriminator(nn.Module):
     def __init__(self, hidden_size: int) -> None:
         super(Discriminator, self).__init__()
-        self.mlp = MLP(embedding_size=768, hidden_size=hidden_size)
-        self.code_bert = CodeBert()
+        self.mlp = MLP(embedding_size=MAX_FEATURE_SIZE, hidden_size=hidden_size)
 
     def forward(self, inputs):
-        output_y0 = self.mlp(inputs[:, 0])
-        output_y1 = self.mlp(inputs[:, 1])
+        features, attention_masks = inputs
+        output_y0 = self.mlp(features[:, 0])
+        output_y1 = self.mlp(features[:, 1])
 
         return torch.tanh(output_y0 - output_y1)
 
@@ -120,8 +134,7 @@ def train(model, train_loader, epochs, lr):
 def collate_fn(batch):
     code_bert = CodeBert()
 
-
-    features = torch.zeros((len(batch), 2, 768))
+    dataset = []
 
     for i, element in enumerate(batch):
         x, y_0, y_1 = element["func_documentation_string"], element["func_code_string"], element["prediction"]
@@ -129,13 +142,28 @@ def collate_fn(batch):
         x_y0 = x + y_0
         x_y1 = x + y_1
 
-        x_y0_enc = torch.tensor(code_bert.get_sentence_embedding(x_y0))
-        x_y1_enc = torch.tensor(code_bert.get_sentence_embedding(x_y1))
+        x_y0_enc = code_bert.get_sentence_embedding(x_y0)
+        x_y1_enc = code_bert.get_sentence_embedding(x_y1)
 
-        features[i, 0] = x_y0_enc
-        features[i, 1] = x_y1_enc
+        dataset.append((x_y0_enc, x_y1_enc))
 
-    return features
+    # max_len = max([max(len(elem[0]), len(elem[1])) for elem in dataset])
+    max_len = MAX_FEATURE_SIZE
+
+    features = torch.zeros((len(dataset), 2, max_len))
+    for i, (f1, f2) in enumerate(dataset):
+        features[i, 0, :len(f1)] = torch.tensor(f1)
+        features[i, 1, :len(f2)] = torch.tensor(f2)
+
+    attention_mask = torch.zeros((len(dataset), 2, max_len))
+
+    original_lengths = torch.tensor([(len(seq[0]), len(seq[1])) for seq in dataset], dtype=torch.float)
+    indices = torch.arange(max_len).unsqueeze(0).unsqueeze(1)
+
+    mask = indices < original_lengths.unsqueeze(2)
+    attention_mask[mask] = 1
+
+    return features, attention_mask
 
 
 if __name__ == '__main__':
@@ -145,3 +173,4 @@ if __name__ == '__main__':
 
     discriminator = Discriminator(args.hidden_size)
     train(discriminator, train_loader, args.epochs, args.optim_lr)
+
