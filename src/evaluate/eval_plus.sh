cdd146aa5b4d5cc2a8d9279df495278a9cc1737a
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-05-12 17:53:07 +0200

    Update eval plus run script

--- src/evaluate/eval_plus.sh
+++ src/evaluate/eval_plus.sh
@@ -8,5 +8,5 @@ dataset="mbpp"
 
 
 python3 src/evaluate/evalp_collect_predictions.py --predictions_file_name="$predictions_file_name" --model_name="$model_name" --path_to_prompt="$path_to_prompt" --language="$language"
-python3 evalplus.sanitize --samples predictions_file_name
-python3 evalplus.evaluate --samples predictions_file_name --dataset $dataset
+evalplus.sanitize --samples "$predictions_file_name"
+evalplus.evaluate --samples "$predictions_file_name" --dataset $dataset
