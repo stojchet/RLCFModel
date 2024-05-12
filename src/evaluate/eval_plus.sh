f58155b09ec6040f33cbcfcf2b875010ba67c809
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-05-12 17:15:58 +0200

    Update eval plus run script

--- src/evaluate/eval_plus.sh
+++ src/evaluate/eval_plus.sh
@@ -4,21 +4,9 @@ predictions_file_name="$1"
 model_name="$2"
 path_to_prompt="$3"
 language="$4"
-
-case $language in
-    "python")
-        dataset="mbpp"
-        ;;
-    "java")
-        dataset="mbjp"
-        ;;
-    *)
-        echo "Unknown language"
-        exit 1
-        ;;
-esac
+dataset="mbpp"
 
 
 python3 src/evaluate/evalp_collect_predictions.py --predictions_file_name="$predictions_file_name" --model_name="$model_name" --path_to_prompt="$path_to_prompt" --language="$language"
-python3 tools/sanitize.py --samples predictions_file_name --dataset $dataset
+python3 evalplus.sanitize --samples predictions_file_name
 python3 evalplus.evaluate --samples predictions_file_name --dataset $dataset
