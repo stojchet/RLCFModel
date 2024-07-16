48d16056f238b4329524578bb32f81427a519c5c
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-07-16 19:23:41 +0200

    Make work with code t5 and add predict single

--- src/model_impl.py
+++ src/model_impl.py
@@ -2,14 +2,14 @@ import gc
 from typing import Union, List
 
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, T5ForConditionalGeneration
 
 
 class Model:
     def __init__(self,
                  name: str,
                  torch_dtype: torch.dtype = torch.float32,
-                 max_new_tokens: int = 2000,
+                 max_new_tokens: int = 1000,
                  padding: Union[bool, str] = False,
                  truncation: Union[bool, str] = False) -> None:
         print(torch_dtype)
@@ -17,16 +17,44 @@ class Model:
         self.max_new_tokens = max_new_tokens
         self.padding = padding
         self.truncation = truncation
-        self.model = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, torch_dtype=torch_dtype)
-        self.tokenizer = AutoTokenizer.from_pretrained(name, trust_remote_code=True, use_fast=True)
 
-        if self.tokenizer.pad_token is None:
-            self.tokenizer.pad_token = self.tokenizer.eos_token
+        if self.__is_code_t5():
+            self.model = T5ForConditionalGeneration.from_pretrained(name)
+        else:
+            self.model = AutoModelForCausalLM.from_pretrained(name, trust_remote_code=True, torch_dtype=torch_dtype)
+        self.tokenizer = AutoTokenizer.from_pretrained(name, trust_remote_code=True, use_fast=True)
+        self.tokenizer.padding_side = "left"
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.model.config.pad_token_id = self.model.config.eos_token_id
+
+    def predict_single(self, prompt: str) -> str:
+        if self.__is_code_t5():
+            prompt = prompt + "<extra_id_0>"
+
+        inputs = self.tokenizer(prompt, return_tensors="pt", return_attention_mask=False, truncation=self.truncation,
+                                padding=self.padding)
+
+        outputs = self.model.generate(**inputs,
+                                      max_new_tokens=self.max_new_tokens,
+                                      do_sample=True,
+                                      pad_token_id=self.tokenizer.pad_token_id
+                                      )
+        result = self.tokenizer.batch_decode(outputs)[0]
+
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        return result
 
     def predict(self, prompts: List[str]) -> str:
-        tokenized = self.tokenizer(prompts, return_tensors="pt", return_attention_mask=False, truncation=self.truncation, padding=True)
+        if self.__is_code_t5():
+            prompts = [prompt + "<extra_id_0>" for prompt in prompts]
 
-        outputs = self.model.generate(tokenized['input_ids'],
+        # attention mask
+        tokenized = self.tokenizer(prompts, return_tensors="pt", return_attention_mask=False,
+                                   truncation=self.truncation, padding=True)
+
+        outputs = self.model.generate(**tokenized,
                                       max_new_tokens=self.max_new_tokens,
                                       do_sample=True,
                                       pad_token_id=self.tokenizer.pad_token_id,
@@ -37,3 +65,6 @@ class Model:
         gc.collect()
 
         return result
+
+    def __is_code_t5(self):
+        return "codet5" in self.name.lower()
