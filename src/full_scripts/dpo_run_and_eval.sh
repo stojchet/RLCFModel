b78e48ce919604ba7d6dc092bbdd1fb0ecfed25a
Teodora Stojcheska <teodora.stojcheska@jetbrains.com>
2024-07-17 20:48:14 +0200

    fix dpo sh

--- src/full_scripts/dpo_run_and_eval.sh
+++ src/full_scripts/dpo_run_and_eval.sh
@@ -16,6 +16,6 @@ python3 src/model/sft_model.py --config_path="$sft_config_path" --config_name="$
 echo "COLLECTING PREDICTIONS DPO"
 python3 src/evaluate/collect_predictions.py --config_path="$config_path" --config_name="$config_name"
 echo "COLLECTING PREDICTIONS"
-python3 src/evaluate/collect_predictions.py --config_path="$config_path" --config_name="$config_name" --add_config_path="$config_path" --add_config_name="$config_name" --path_to_prompt="$path_to_prompt" --batch_size="$batch_size"
+python3 src/evaluate/collect_predictions.py --config_path="$sft_config_path" --config_name="$sft_config_name" --add_config_path="$config_path" --add_config_name="$config_name" --path_to_prompt="$path_to_prompt" --batch_size="$batch_size"
 echo "RUNNING EVALUATION"
 python3 src/evaluate/run_eval.py --config_path="$config_path" --config_name="$config_name" --prompt_path="$path_to_prompt"
